dc91d7699601650dd7375beb3332ab39930c86fa
Rye-007 <sulaiman.weani@gmail.com>
2022-07-19 14:19:32 +0700

    second

---
+++ k.dart
@@ -0,0 +1,74 @@
+import 'dart:ffi';
+import 'dart:io' show stdin, stdout;
+
+int main() {
+  double? numOne, numTwo, res;
+  int? choice;
+  do {
+    stdout.write("1. Addition\n");
+    stdout.write("2. Subtraction\n");
+    stdout.write("3. Multiplication\n");
+    stdout.write("4. Division\n");
+    stdout.write("5. Exit\n\n");
+    stdout.write("Enter Your Choice(1-5): ");
+    choice = int.parse(stdin.readLineSync()!);
+    if (choice! >= 1 && choice <= 4) {
+      stdout.write("\nEnter any two Numbers:");
+      numOne = double.parse(stdin.readLineSync()!);
+      stdout.write("\nEnter any two Numbers:");
+      numTwo = double.parse(stdin.readLineSync()!);
+    }
+    switch (choice) {
+      case 1:
+        res = addFun(numOne!, numTwo!);
+        stdout.write("\nResult = ${res}");
+        break;
+      case 2:
+        res = subFun(numOne!, numTwo!);
+        stdout.write("\nResult = ${res}");
+        break;
+      case 3:
+        res = mulFun(numOne!, numTwo!);
+        stdout.write("\nResult = ${res}");
+        break;
+      case 4:
+        res = divFun(numOne!, numTwo!);
+        stdout.write("\nResult = ${res}");
+        break;
+      case 5:
+        return 0;
+      default:
+        stdout.write("\nResult = ${res}");
+        break;
+    }
+    stdout.write("\n------------------------\n");
+  } while (choice != 5);
+  return 0;
+}
+
+double addFun(double a, double b) {
+  return (b + a);
+}
+
+double subFun(double a, double b) {
+  return (a - b);
+}
+
+double mulFun(double a, double b) {
+  return (a * b);
+}
+
+double divFun(double a, double b) {
+  return (a / b);
+}//1
+เท็น — 28/06/2022
+ข้อ 5
+import 'dart:io' show stdin,stdout;
+
+int main (List<String> arguments){
+  int binaryNum,decimalNum;
+  stdout.write('Enter the Decimal Number: ');
+  decimalNum = int.parse(stdin.readLineSync()!);
+  binaryNum = DecimalTobinary(decimalNum);
+  print('Equivalent Binary Value: ${binaryNum}');
+  return 
